c164d88774c19a2f758d731a8629a8ef488d31dc
Sandeep Shome <sandeepshome10@gmail.com>
2026-02-11 18:34:21 +0530

    feat: export logger class and types

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { LogOptions } from './types/index';
+import { emitLog } from './libs/logger';
+
+export class Logger {
+  public log = (msg: unknown, options?: LogOptions) => emitLog(msg, options);
+  public createLogger = (options?: LogOptions) => (msg: unknown) =>
+    emitLog(msg, options);
+}
+export * from './types/index';
